c4eae5c44476a884f6cc6e34fabe07b5eb5629f7
icusT <105977404+icusT@users.noreply.github.com>
2022-07-17 13:27:35 +0300

    Update main2.js

--- main2.js
+++ main2.js
@@ -320,7 +320,7 @@ function END() {
 
                         $("#CTEXT").addClass("animate__" + TRANSITION1 + "Out");
 
-                        $("#FINAL_IMG").attr("src", "https://d13pxqgp3ixdbh.cloudfront.net/uploads/1607760432ad31b43e2d7288a888438b0668454760.png");
+                        $("#FINAL_IMG").attr("src", "https://d13pxqgp3ixdbh.cloudfront.net/uploads/1652968941ff5a381dbcfde298b7ccc69ea92ea9cd.pn");
 
                         $("#FINAL_VB").text(SELECTED_VB);
 
